c850fa4d7b1c2e416887829537da91079a9b0496
Josh Ellison <joshellison@Jeremiahs-Mac-mini.local>
2019-04-15 07:50:41 -0700

    buttons added

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
 console.log('sheets linked');
+
+
+$(() => {
+
+    $('form').on('submit', (event) => {
+        event.preventDefault();
+
+        const userInput = $('input[type="text"]').val();
+
+        $.ajax(
+            {
+                url:'http://www.omdbapi.com/?apikey=53aa2cd6&t=' + userInput,
+            }
+        ).then(
+            (data) => {
+                console.log(data);
+                $('#title').html(data.Title)
+                $('#year').html(data.Year)
+                $('#rating').html(data.Rated)
+            },
+            () => {
+                console.log('bad request');
+            }
+        );
+
+    });
+
+
+})
